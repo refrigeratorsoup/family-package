ca46d01c8c4206641effa2deb89d68ee0290237c
refrigeratorsoup <117627620+refrigeratorsoup@users.noreply.github.com>
2022-11-09 13:23:28 +0700

    made scripts for astro

---
+++ alien_project/Assets/Scripts/astroScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class astroScript : MonoBehaviour
+{
+    Rigidbody2D Rigidbody2D
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
