ca113749368a49b58a69af30ef11204c4294864f
refrigeratorsoup <117627620+refrigeratorsoup@users.noreply.github.com>
2022-11-29 14:21:57 +0700

    slkme health

--- alien_project/Assets/SlimeScript.cs
+++ alien_project/Assets/SlimeScript.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class SlimeScript : MonoBehaviour
 {
+    public float slimeHealth;
+    public float slimeMaxHealth = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        slimeHealth = slimeMaxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    void GetShot()
     {
-        
+        slimeHealth -= 1;
+        if (slimeHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D (Collision2D col)
     {
         if (col.gameObject.tag.Equals("Bullet"))
         {
+            GetShot();
             Destroy(col.gameObject);
-            Destroy(gameObject);
         }
     }
 
